a7fc2640b6604f9061c593635429ae493b92a3db
Alex <46602330+AlexEra@users.noreply.github.com>
2019-06-19 15:52:24 +0300

    Update undist.py

--- undist.py
+++ undist.py
@@ -6,32 +6,34 @@ import pickle
 # Generation of checker board
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_5X5_100)
 parameters = aruco.DetectorParameters_create()
-board = aruco.CharucoBoard_create(4, 4, 0.045, 0.03, dictionary) # squaresX, squaresY, squareLength, markerLength, dictionary
+board = aruco.CharucoBoard_create(4, 4, 0.045, 0.03, dictionary) 
 
-# load coeffs
+# Load coeffs.
 with open('cam_param.pkl', 'rb') as f:
     camera_param = pickle.load(f)
-cameraMatrix, distCoeffs, rvecs, tvecs, stdDeviationsInstrinsics, stdDeviationsExtrinsics = camera_param
+cameraMatrix, distCoeffs, rvecs, tvecs, stdDeviationsInstrinsics, 
+stdDeviationsExtrinsics = camera_param
 
 calibImages = []
 count = 0
 while count < 6:
-    calibImage = cv2.imread('res'+str(count)+'.png')
+    calibImage = cv2.imread('res' + str(count) + '.png')
     count += 1
     if calibImage is None:
         break
-    calibImages.append(calibImage) # save the loaded image
+    calibImages.append(calibImage)  # Save the loaded image.
     
-# size of img
-h,  w = calibImages[0].shape[:2]
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix,distCoeffs,(w,h),1,(w,h))
+h,  w = calibImages[0].shape[:2]  # Size of img
+newcameramtx, roi = cv2.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs,
+                                                  (w, h), 1, (w, h))
 for i, before_undistortImg in enumerate(calibImages):
-    # undistort
+    # Undistort
     dst = cv2.undistort(before_undistortImg, cameraMatrix, distCoeffs, None, newcameramtx)
     cv2.imshow('undistort', dst)
     cv2.waitKey(0)
-    # crop the image
-    x,y,w,h = roi
+    
+    # Crop the image.
+    x, y, w, h = roi
     dst = dst[y:y+h, x:x+w]
-    # write the new image
-    cv2.imwrite('calibresult'+ str(i) +'.png',dst)
+    # Write the new image.
+    cv2.imwrite('calibresult' + str(i) + '.png', dst)
